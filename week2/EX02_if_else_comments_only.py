fe0cc8b539f3953c16eab9d41434d8f975931dd8
Devansh <DEVMAN89658@nbss.nbed.nb.ca>
2026-02-08 13:51:37 -0400

    Update EX02_if_else_comments_only.py

--- week2/EX02_if_else_comments_only.py
+++ week2/EX02_if_else_comments_only.py
@@ -1,16 +1,9 @@
-# Filename: EX02_if_else_comments_only.py
-# EX02 - if / else (COMMENTS ONLY)
-#
-# Goal:
-#   Write a program that asks:
-#     "Is it Sunday?" (yes/no)
-#
-# If yes:
-#   print("Math and Coding Day!")
-# Else:
-#   print("Can't wait for Sunday...")
-#
-# Rules:
-#   - Use input() once
-#   - Use .strip().lower()
-#   - Use one if and one else
+# Devansh Week 2 EXO2
+print("=== if / else ===")
+
+answer = input("Is it Sunday? (yes/no): ").strip().lower()
+
+if answer == "yes":
+    print("Math and Coding Day!")
+else:
+    print("Can't wait for Sunday...")
