f22163d3a94feb5828588b339133ee9025dc5baa
Devansh <DEVMAN89658@nbss.nbed.nb.ca>
2026-02-09 20:23:44 -0400

    Update EX04_boolean_logic_comments_only.py

--- week2/EX04_boolean_logic_comments_only.py
+++ week2/EX04_boolean_logic_comments_only.py
@@ -1,18 +1,7 @@
-# Filename: EX04_boolean_logic_comments_only.py
-# EX04 - Boolean logic (COMMENTS ONLY)
-#
-# Goal:
-#   Make a “pool rules” checker.
-#
-# Ask:
-#   - Age (int)
-#   - Can you swim? (yes/no)
-#
-# Rules:
-#   - If age >= 10 AND can_swim is yes -> "You can go in the deep end."
-#   - Otherwise -> "Stay in the shallow end."
-#
-# Hint:
-#   can_swim = input("Can you swim? ").strip().lower()
-#   if age >= 10 and can_swim == "yes":
-#       ...
+#by Devansh
+can_swim = input("Can you swim? ").strip().lower()
+age = input("What age are you? ").strip().lower()
+if age==10 and can_swim == "yes":
+  print("You can go in the deep end.")
+else:
+  print("Stay in the shallow end.")
